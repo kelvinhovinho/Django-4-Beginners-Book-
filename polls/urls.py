d21881ada8ec61bc25c1a14a97d5b4d14db5de7c
Akelvino <vinho892@gmail.com>
2025-03-12 22:12:28 +0300

    Added the query from the db

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    # path('<int:question_id/>',views.details, name='details'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/votes/', views.vote, name='vote'),  
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello, World!!!</h1>")
+    latest_question_list = Question.objects.order_by('pub_date')[:5] 
+    return render(request, 'polls/index.html', {"latest_question_list":latest_question_list})
+
+def details(request,question_id):
+    pass
+
+def results(request, question_id):
+    pass
+
+def vote(request, question_id):
+    pass
